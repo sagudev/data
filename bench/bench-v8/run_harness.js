73c9da9cfa43477bdf2550682f0be7caede60e96
jasonwilliams <jase.williams@gmail.com>
2024-03-31 18:35:26 +0100

    add benchmarks from other engines

--- bench/bench-v8/run_harness.js
+++ bench/bench-v8/run_harness.js
@@ -2,41 +2,47 @@
 var print = console.log;
 
 function Run() {
-  BenchmarkSuite.RunSuites({ NotifyStep: ShowProgress,
-                             NotifyError: AddError,
-                             NotifyResult: AddResult,
-                             NotifyScore: AddScore });
+  BenchmarkSuite.RunSuites({
+    NotifyStep: ShowProgress,
+    NotifyError: AddError,
+    NotifyResult: AddResult,
+    NotifyScore: AddScore,
+  });
 }
 
 var harnessErrorCount = 0;
 
 function ShowProgress(name) {
-  print('PROGRESS', name);
+  // print("PROGRESS", name);
 }
 
 function AddError(name, error) {
-  print('ERROR', name, error);
+  print("ERROR", name, error);
   print(error.stack);
   harnessErrorCount++;
 }
 
 function AddResult(name, result) {
-  print('RESULT', name, result);
+  // print("RESULT", name, result);
+  // Print result in JSON format
+  print(`  "${name}": ${result},`);
 }
 
 function AddScore(score) {
-  print('SCORE', score);
+  print(`  "score": ${score}`);
+  print("}");
 }
 
 try {
+  print("{");
   Run();
 } catch (e) {
-  print('*** Run() failed');
+  print("*** Run() failed");
   print(e.stack || e);
 }
 
 if (harnessErrorCount > 0) {
   // Throw an error so that 'duk' has a non-zero exit code which helps
   // automatic testing.
-  throw new Error('Benchmark had ' + harnessErrorCount + ' errors');
+  throw new Error("Benchmark had " + harnessErrorCount + " errors");
 }
