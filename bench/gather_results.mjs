4062d6a45475f4f98c121518f9a6539b2716b439
jasonwilliams <jase.williams@gmail.com>
2024-04-01 17:50:44 +0100

    create template for results data

--- bench/gather_results.mjs
+++ bench/gather_results.mjs
@@ -1,10 +1,28 @@
 import fs from "node:fs";
+
 const engines = new Map();
+// engines.set("boa", {});
 engines.set("v8-jitless", {});
+engines.set("libjs", {});
+// engines.set("duktape", {});
+// engines.set("quickjs", {});
+
+const benchmarks = [
+  "Richards",
+  "DeltaBlue",
+  "Crypto",
+  "RayTrace",
+  "EarleyBoyer",
+  "RegExp",
+  "Splay",
+  "NavierStokes",
+  "score",
+];
 
 const resultsRegex = new RegExp('"?RESULT"?\\s"?(\\w+)"?\\s"?(\\d+)"?');
 const scoreRegex = new RegExp('"?SCORE"?\\s"?(\\d+)"?');
 
+// gather data from txt files
 engines.forEach((val, engine) => {
   const results = fs.readFileSync(`./bench/${engine}_results.txt`).toString();
   const lines = results.split("\n");
@@ -16,4 +34,15 @@ engines.forEach((val, engine) => {
   val["score"] = parseInt(scoreRegex.exec(results)[1]);
 });
 
-console.log(engines.get("v8-jitless"));
+// generate JSON files for each benchmark
+benchmarks.forEach((benchmark) => {
+  const data = fs.readFileSync(`./bench/results/${benchmark}.json`);
+  engines.forEach((val, engine) => {
+    data["results"][engine].push(val[benchmark]);
+  });
+  data["labels"].push(new Date().getTime());
+  fs.writeFileSync(
+    `./bench/results/${benchmark}.json`,
+    JSON.stringify(data, null, 2)
+  );
+});
